3d8b95ff6d27d118355f3b211415d41436d89e00
ShaneOH <shane.ohanlon93@gmail.com>
2016-05-17 21:39:49 -0400

    retab

--- estimates.py
+++ estimates.py
@@ -3,31 +3,31 @@ import json
 import config
 
 class Uber:
-	url = 'https://api.uber.com/v1/estimates'
-	
-	def __init__(self):
-		self.auth = {'Authorization': 'Token ' + config.uberToken}
+    url = 'https://api.uber.com/v1/estimates'
+    
+    def __init__(self):
+        self.auth = {'Authorization': 'Token ' + config.uberToken}
 
 class Lyft:
-	url = 'https://api.lyft.com/v1'
+    url = 'https://api.lyft.com/v1'
 
-	def __init__(self):
-		self.auth = self.getToken()
+    def __init__(self):
+        self.auth = self.getToken()
 
-	def getToken(self):
-		i = 1;		
+    def getToken(self):
+        i = 1;      
 
 class Estimate:
-	lyftUrl = 'https://api.lyft.com/v1'
-	lyftAuth = {'Authorization': 'Bearer ' + config.token}
-	location = {'start_latitude': lat, 'start_longitude': long}
+    lyftUrl = 'https://api.lyft.com/v1'
+    lyftAuth = {'Authorization': 'Bearer ' + config.token}
+    location = {'start_latitude': lat, 'start_longitude': long}
 
-	#r = req.get(uberUrl+'/time', headers=uberAuth, params=location)
-	#uberTime = json.loads(r.text)
+    #r = req.get(uberUrl+'/time', headers=uberAuth, params=location)
+    #uberTime = json.loads(r.text)
 
 def main():
-	print "Hello!"
+    print "Hello!"
 
 if __name__ == '__main__':
-	main()
+    main()
 
