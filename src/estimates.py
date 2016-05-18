a3d7dfb8e32c2a8396e19dd36aa5dbe761b24028
ShaneOH <shane.ohanlon93@gmail.com>
2016-05-17 20:58:40 -0400

    refactor estimates into classes

--- src/estimates.py
+++ src/estimates.py
@@ -2,12 +2,32 @@ import requests as req
 import json
 import config
 
-url = 'https://api.uber.com/v1/estimates'
-lat = '40.740308'
-long = '-73.985428'
-headers = {'Authorization': 'Token ' + config.token}
-location = {'start_latitude': lat, 'start_longitude': long}
+class Uber:
+	url = 'https://api.uber.com/v1/estimates'
+	
+	def __init__(self):
+		self.auth = {'Authorization': 'Token ' + config.uberToken}
 
-r = req.get(url+'/time', headers=headers, params=location)
-estimates = json.loads(r.text)
+class Lyft:
+	url = 'https://api.lyft.com/v1'
+
+	def __init__(self):
+		self.auth = self.getToken()
+
+	def getToken(self):
+		i = 1;		
+
+class Estimate:
+	lyftUrl = 'https://api.lyft.com/v1'
+	lyftAuth = {'Authorization': 'Bearer ' + config.token}
+	location = {'start_latitude': lat, 'start_longitude': long}
+
+	#r = req.get(uberUrl+'/time', headers=uberAuth, params=location)
+	#uberTime = json.loads(r.text)
+
+def main():
+	print "Hello!"
+
+if __name__ == '__main__':
+	main()
 
