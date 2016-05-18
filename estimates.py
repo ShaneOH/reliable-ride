37664027261b6529006b8ff18e9c3b92e993eb85
ShaneOH <shane.ohanlon93@gmail.com>
2016-05-18 00:16:31 -0400

    lyft auth working

--- estimates.py
+++ estimates.py
@@ -15,18 +15,25 @@ class Lyft:
         self.auth = self.getToken()
 
     def getToken(self):
-        i = 1;      
+        try:
+            token = config.lyftToken
+        except AttributeError:
+            headers = {'Content-Type': 'application/json'}
+            data = '{"grant_type": "client_credentials", "scope": "public"}'
+            r = req.post('https://api.lyft.com/oauth/token', headers=headers, data=data, auth=(config.lyftID, config.lyftSecret))
+            token = json.loads(r.text)
+        return token
 
 class Estimate:
-    lyftUrl = 'https://api.lyft.com/v1'
-    lyftAuth = {'Authorization': 'Bearer ' + config.token}
-    location = {'start_latitude': lat, 'start_longitude': long}
+    pass
+    #location = {'start_latitude': lat, 'start_longitude': long}
 
     #r = req.get(uberUrl+'/time', headers=uberAuth, params=location)
     #uberTime = json.loads(r.text)
 
 def main():
-    print "Hello!"
+    lyft = Lyft()
+    print lyft.auth
 
 if __name__ == '__main__':
     main()
