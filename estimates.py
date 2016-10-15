74038d4574ced5e433f9eada901318642e11ce94
ShaneOH <shane.ohanlon93@gmail.com>
2016-10-14 20:01:02 -0400

    cleaning; open places file properly

--- estimates.py
+++ estimates.py
@@ -1,5 +1,5 @@
 import requests as req
-import json
+import json, sys, os, csv
 
 with open('config.json', 'r') as f:
     config = json.load(f)
@@ -7,19 +7,31 @@ with open('config.json', 'r') as f:
 uber = {'url': 'https://api.uber.com/v1/estimates/time'};
 lyft = {'url': 'https://api.lyft.com/v1/eta'};
 
-def main();
-    authUber()
+def main():
     authLyft()
-    with open(os.path.join(os.path.dirname(__file__), './data/places')) as place:
+    authUber()
+    with open(os.path.join(os.path.dirname(__file__), './data/places.csv')) as places:
+        for place in csv.reader(places, delimiter = ','):
+            location = {'start_latitude': place[2], 'start_longitude': place[3]}
 
-def lyftAuth():
+
+def authLyft():
     headers = {'Content-Type': 'application/json'}
     data = '{"grant_type": "client_credentials", "scope": "public"}'
     r = req.post('https://api.lyft.com/oauth/token', headers=headers, data=data, auth=(config['lyftID'], config['lyftSecret']))
     token = json.loads(r.text)
     lyft['token'] = token['access_token']
 
-if __name__ = '__main__':
+def authUber():
+    pass
+
+def etaLyft():
+    pass
+
+def etaUber():
+    pass
+
+if __name__ == '__main__':
     main()
    
 
